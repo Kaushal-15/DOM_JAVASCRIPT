dbb84e0d45868ab232bbf6f6ad237e449706db70
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-07-22 19:30:52 +0530

    file added

---
+++ getElementByname.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <p id="id">HELLO CODER'S</p>
+    </div>
+    <input type="radio" name="language" value="js">
+    <input type="radio" name="language" value="c">
+    <input type="radio" name="language" value="python">
+    <script>
+        let name=document.getElementsByName("language");
+        console.log(name);
+    </script>
+</body>
+</html>
