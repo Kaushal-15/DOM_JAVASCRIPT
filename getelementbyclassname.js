1ea5e7acfd4bca870c907fe1df1a27b8e78e87ee
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-07-22 20:34:48 +0530

    file added

---
+++ getelementbyclassname.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+<h1 class="class">First heading</h1>
+<h1>second heading</h1>
+<h1>third heading</h1>
+    <script>
+        let Class = document.getElementsByClassName("class");
+        let heading = document.getElementsByTagName("h1");
+        console.log(heading);
+     console.log(Class);    
+
+    </script>
+</body>
+</html>     
